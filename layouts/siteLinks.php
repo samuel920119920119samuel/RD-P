0b79eee1ef110f40e881fb43e3d65b0f7a2d8f62
root <root@RD-P.v3197.ncu>
2018-04-07 22:43:05 +0800

    update side-bar links

--- layouts/siteLinks.php
+++ layouts/siteLinks.php
@@ -3,13 +3,12 @@
             col-sm-offset-4 col-sm-4 col-sm-offset-4
             col-xs-offset-3 col-xs-6 col-xs-offset-3" >
 <p class="link">研發處企劃組</p>
-<a href="http://www.ncu.edu.tw/~ncu7020/AboutRDO/business.php#1" class="btn btn-default btn-sm" role="button">本組簡介</a>
-<a href="http://www.ncu.edu.tw/~ncu7020/AboutRDO/member.php#2" class="btn btn-default btn-sm" role="button">成員及職掌</a>
-<a href="http://www.ncu.edu.tw/~ncu7020/Files/%E4%BC%81%E5%8A%83%E7%B5%84FAQ(103%E5%B9%B4%E7%89%88).docx" class="btn btn-default btn-sm" role="button">FAQ</a>
-<a href="http://www.ncu.edu.tw/~ncu7020/Project/index.php" class="btn btn-default btn-sm" role="button">校務發展計畫</a>
-<a href="http://www.ncu.edu.tw/~ncu7020/Project/record.php" class="btn btn-default btn-sm" role="button">校務發展委員會議</a>
-<a href="http://www.ncu.edu.tw/~ncu7020/Project/newCampus.php" class="btn btn-default btn-sm " role="button">八德校地籌設</a>
-<a href="https://ncu.edu.tw/assessment/" class="btn btn-default btn-sm " role="button">校務評鑑</a>
-<a href="http://www.ncu.edu.tw/~ncu7020/Project/integrationGuide.php" class="btn btn-default btn-sm " role="button">教育部統合視導</a>
+<a href="https://ncu.edu.tw/rd/tw/page/index.php?root=5&num=22#2" class="btn btn-default btn-sm" role="button">本組簡介</a>
+<a href="https://ncu.edu.tw/rd/tw/page/index.php?root=5&num=32#2" class="btn btn-default btn-sm" role="button">成員及職掌</a>
+<a href="https://ncu.edu.tw/rd/tw/page/index.php?root=6&num=26" class="btn btn-default btn-sm" role="button">校務發展計畫</a>
+<a href="https://ncu.edu.tw/rd/tw/page/index.php?root=6&num=27" class="btn btn-default btn-sm" role="button">校務發展委員會議</a>
+<a href="https://ncu.edu.tw/rd/tw/page/index.php?root=6&num=25" class="btn btn-default btn-sm " role="button">八德校地籌設</a>
+<a href="https://ncu.edu.tw/assessment/news" class="btn btn-default btn-sm " role="button">校務評鑑</a>
+<a href="https://ncu.edu.tw/rd/tw/page/index.php?root=6&num=28" class="btn btn-default btn-sm " role="button">教育部統合視導</a>
 <a href="/issue" class="btn btn-default btn-sm active" role="button">高教潛力議題觀測站</a>
 </div>
