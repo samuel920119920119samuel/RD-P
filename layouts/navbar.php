f8ed07a297006a5b4390b8c00199908a0058efd2
root <root@RD-P.v3197.ncu>
2018-04-12 00:43:49 +0800

    update link

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -14,25 +14,23 @@
                <button type="button" class="navbar-toggle" style="color:white; font-family: Microsoft JhengHei"; >高教潛力議題觀測站</button> 
           </div>
           <ul class="nav navbar-nav collapse navbar-collapse" id="app-navbar-collapse">
-              <li><a href="http://www.ncu.edu.tw/~ncu7020/index.php">研發處</a></li>
+              <li><a href="https://ncu.edu.tw/rd/">研發處</a></li>
               <li class="dropdown active"><a class="dropdown-toggle" data-toggle="dropdown" href="#">企劃組<span class="caret"></span></a>
                   <ul class="dropdown-menu">
-                      <li><a href="http://www.ncu.edu.tw/~ncu7020/AboutRDO/business.php#1">本組簡介</a></li>
-                      <li><a href="http://www.ncu.edu.tw/~ncu7020/AboutRDO/member.php#2">成員及職掌</a></li>
-                      <li><a href="http://www.ncu.edu.tw/~ncu7020/Files/%E4%BC%81%E5%8A%83%E7%B5%84FAQ(103%E5%B9%B4%E7%89%88).docx">FAQ</a></li>
-                      <li><a href="http://www.ncu.edu.tw/~ncu7020/Project/index.php">校務發展計畫</a></li>
-                      <li><a href="http://www.ncu.edu.tw/~ncu7020/Project/record.php">校務發展委員會議</a></li>
-                      <li><a href="http://www.ncu.edu.tw/~ncu7020/Project/newCampus.php">八德校地籌設</a></li>
+                      <li><a href="https://ncu.edu.tw/rd/tw/page/index.php?root=5&num=22#2">本組簡介</a></li>
+                      <li><a href="https://ncu.edu.tw/rd/tw/page/index.php?root=5&num=32#2">成員及職掌</a></li>
+                      <li><a href="https://ncu.edu.tw/rd/tw/page/index.php?root=6&num=26">校務發展計畫</a></li>
+                      <li><a href="https://ncu.edu.tw/rd/tw/page/index.php?root=6&num=27">校務發展委員會議</a></li>
+                      <li><a href="https://ncu.edu.tw/rd/tw/page/index.php?root=6&num=25">八德校地籌設</a></li>
                       <li><a href="https://ncu.edu.tw/assessment/">校務評鑑</a></li>
-                      <li><a href="http://www.ncu.edu.tw/~ncu7020/Project/integrationGuide.php">教育部統合視導</a></li>
+                      <li><a href="https://ncu.edu.tw/rd/tw/page/index.php?root=6&num=28">教育部統合視導</a></li>
                       <li class="active"><a href="/issue">高教潛力議題觀測站</a></li>
                   </ul>
               </li>
-              <li><a href="http://www.ncu.edu.tw/~ncu7020/Research/">研究推動組</a></li>
-              <li><a href="http://tlo.ncu.edu.tw/portal/">智權技轉組</a></li>
-              <li><a href="http://www.iic.ncu.edu.tw/">創新育成中心</a></li>
-              <li><a href="http://www.ncu.edu.tw/~ncu7020/Instrument/">貴重儀器中心</a></li>
-              <li><a href="http://www.ncu.edu.tw/~ncu7020/EEC/">環境教育中心</a></li>
+              <li><a href="https://ncu.edu.tw/rd/tw/index/index.php?root=7">研究推動組</a></li>
+              <li><a href="http://www.caic.ncu.edu.tw/index.php?lang=tw">產學營運中心</a></li>
+              <li><a href="https://ncu.edu.tw/rd/tw/index/index.php?root=9">貴重儀器中心</a></li>
+              <li><a href="https://ncu.edu.tw/rd/tw/index/index.php?root=10">環境教育中心</a></li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
             <li> <a href="login.php" ng-click="dataC.test()"><span class="glyphicon glyphicon-log-in"></span>&nbsp;Login</a></li>
